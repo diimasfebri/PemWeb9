cacd9ce3a8bd1dd4898699fd98a23be86abece8d
diimasfebri <dimas.febri21@gmail.com>
2021-05-14 18:45:42 +0700

    PemWeb9

---
+++ koneksi_pendaftaran.php
@@ -0,0 +1,10 @@
+<?php
+//inisialisasi database
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "latihan2";
+
+//koneksi dabase
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+?>
